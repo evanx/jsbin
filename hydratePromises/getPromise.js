fbf0a928b9bdc77f89d7fe292e012c627cd896fe
evans <evans@saratoga.co.za>
2015-03-22 01:31:53 +0200

    hydrate

---
+++ hydratePromises/getPromise.js
@@ -0,0 +1,29 @@
+
+export function getPromise(url) {
+   if (cache[url]) {
+      var data = cache[url];
+      if (data._time)  {
+         if (new Date().getTime() - data._time < config.cacheExpirySeconds*1000) {
+            return Promise.resolve(data);
+         }
+      } else {
+         return Promise.resolve(data);
+      }
+   }
+   return new Promise((resolve, reject) => {
+      var req = new XMLHttpRequest();
+      req.onload = function () {
+         if (req.status !== 200) {
+            reject({message: 'status ' + req.status});
+         } else {
+            var data = JSON.parse(req.response);
+            data._time = new Date().getTime();
+            cache[url] = data;
+            resolve(data);
+         }
+      };
+      req.open('GET', url);
+      req.send();
+      log.debug('loadJSON', url);
+   });
+}
