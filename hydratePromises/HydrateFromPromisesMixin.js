f781169e823750134ea07ca24265d528abf5aedc
evans <evans@saratoga.co.za>
2015-03-22 16:58:32 +0200

    readme

--- hydratePromises/HydrateFromPromisesMixin.js
+++ hydratePromises/HydrateFromPromisesMixin.js
@@ -5,23 +5,17 @@ var debug = function() {
 var HydrateFromPromisesMixin = {
    hydrateFromPromises: function(promises) {
       debug('hydrate', Object.keys(promises));
-      let that = this;
-      let count = 0;
-      function set(key, data) {
-         that.state[key] = data;
-         count += 1;
-         if (count === Object.keys(promises).length) {
-            debug('hydrate resolved');
-            that.setState(that.state);
-         }
-      }
+      let countDownLatch = new CountDownLatch(Object.keys(promises).length, () => {
+         this.setState(this.state);
+      });
       Object.keys(promises).forEach(key => {
          promises[key]().then(data => {
             debug('hydrate promise resolved', key);
-            set(key, data);
-         }, function(error) {
+            this.state[key] = data;
+            countDownLatch.signal();
+         }, error => {
             debug('hydrate promise rejected', key, error);
-            set(key, null);
+            countDownLatch.signal();
          });
       });
    }
